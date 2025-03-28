85b4dd47b0d922b656b551efe13ece528d2c5257
C0dewithawais <awaisalisabir4@gmail.com>
2025-03-28 14:41:49 +0500

    formatting code

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,44 +1,62 @@
-function About(){
-    return(
-      <section id="about">
-        <div class="about-box">
-          <div class="about-info">
-            <h1 class="animate-on-scroll animate__fadeInDownBig"><span>About Me</span></h1>
-            <p class="animate-on-scroll animate__rollIn"> 
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita
-              sapiente quis optio fugit exercitationem illo voluptatem beatae nisi
-              laborum, perspiciatis accusamus qui odit molestias voluptatibus aut
-              et id veritatis distinctio!
-            </p>
-          </div>
-          <h2 class="animate-on-scroll animate__rollIn"><span>Skills</span></h2>
-          <div class="skills animate-on-scroll animate__zoomIn">
-            <ul>
-              <li>
-                <span><i class="bx bx-chevron-right"></i>HTML</span>
-              </li>
-              <li>
-                <span><i class="bx bx-chevron-right"></i>CSS</span>
-              </li>
-              <li>
-                <span><i class="bx bx-chevron-right"></i>Javascript</span>
-              </li>
-            </ul>
-            <ul>
-              <li>
-                <span><i class="bx bx-chevron-right"></i>React</span>
-              </li>
-              <li>
-                <span><i class="bx bx-chevron-right"></i>Node</span>
-              </li>
-              <li>
-                <span><i class="bx bx-chevron-right"></i>Mongodb</span>
-              </li>
-            </ul>
-          </div>
+function About() {
+  return (
+    <section id="about">
+      <div class="about-box">
+        <div class="about-info">
+          <h1 class="animate-on-scroll animate__fadeInDownBig">
+            <span>About Me</span>
+          </h1>
+          <p class="animate-on-scroll animate__rollIn">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita
+            sapiente quis optio fugit exercitationem illo voluptatem beatae nisi
+            laborum, perspiciatis accusamus qui odit molestias voluptatibus aut
+            et id veritatis distinctio!
+          </p>
         </div>
-       <div class="animate-on-scroll animate__lightSpeedInRight"> <img src="Awais(2).jpg" alt="" /></div>
-      </section>
-    )
-  }
-  export default About
+        <h2 class="animate-on-scroll animate__rollIn">
+          <span>Skills</span>
+        </h2>
+        <div class="skills animate-on-scroll animate__zoomIn">
+          <ul>
+            <li>
+              <span>
+                <i class="bx bx-chevron-right"></i>HTML
+              </span>
+            </li>
+            <li>
+              <span>
+                <i class="bx bx-chevron-right"></i>CSS
+              </span>
+            </li>
+            <li>
+              <span>
+                <i class="bx bx-chevron-right"></i>Javascript
+              </span>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <span>
+                <i class="bx bx-chevron-right"></i>React
+              </span>
+            </li>
+            <li>
+              <span>
+                <i class="bx bx-chevron-right"></i>Node
+              </span>
+            </li>
+            <li>
+              <span>
+                <i class="bx bx-chevron-right"></i>Mongodb
+              </span>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="animate-on-scroll animate__lightSpeedInRight">
+        <img src="Awais(2).jpg" alt="" />
+      </div>
+    </section>
+  );
+}
+export default About;
