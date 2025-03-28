d80613a66c18175c401246121b5b84de258ba511
C0dewithawais <awaisalisabir4@gmail.com>
2025-03-28 15:59:48 +0500

    add links on footer icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
           <span>
             <i class="bx bxs-location-plus"></i>
           </span>
-          <span>Street no 10,Muhammadabad,Faisalabad.</span>
+          <span>Street no 10, Muhammadabad, Faisalabad.</span>
         </div>
         <div class="col-box">
           <span>
@@ -28,22 +28,22 @@ function Footer() {
           solutions that help businesses thrive. Whether it's a sleek website, a
           powerful software application, or an efficient business management
           system, I ensure top-notch quality and innovation. 
-          <br></br>💡 Let's bring your
+          <br /><br />💡 Let's bring your
           ideas to life! 🚀 Get in touch today!
         </p>
         <div class="social-icons">
-          <span>
+          <a href="https://www.facebook.com/share/18LVqxpT31/" target="_blank" rel="noopener noreferrer">
             <i class="bx bxl-facebook-circle"></i>
-          </span>
-          <span>
+          </a>
+          <a href="https://instagram.com/https://www.instagram.com/c0dewithawais?igsh=emk0bW1vNmpsdnAw" target="_blank" rel="noopener noreferrer">
             <i class="bx bxl-instagram-alt"></i>
-          </span>
-          <span>
+          </a>
+          <a href="https://www.linkedin.com/in/awais-ali-ads-expert/" target="_blank" rel="noopener noreferrer">
             <i class="bx bxl-linkedin"></i>
-          </span>
-          <span>
+          </a>
+          <a href="https://youtube.com/@codewithawa1s?si=58_dETfSpR_K4G2F" target="_blank" rel="noopener noreferrer">
             <i class="bx bxl-youtube"></i>
-          </span>
+          </a>
         </div>
       </div>
     </footer>
